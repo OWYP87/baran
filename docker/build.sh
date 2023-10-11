58721cd3d134dca1c575da80e85c54eb376a27c0
Artur <szalasartur@gmail.com>
2023-10-11 12:45:32 +0200

    removed part responsible for login into docker as it will be managed by GitHub actions

--- docker/build.sh
+++ docker/build.sh
@@ -23,12 +23,6 @@ echo "Git hash is: $GIT_HASH" >> version.txt
 
 # there is no login - we assume user already had logged into docker registry the main one https://hub.docker.com/
 
-REGISTRY_URL="https://index.docker.io/v1/"
-USERNAME="szalasartur@gmail.com"
-read -s -p "Enter Docker Hub personal access token: " ACCESS_TOKEN
-echo "$ACCESS_TOKEN" | docker login $REGISTRY_URL -u $USERNAME --password-stdin
-unset ACCESS_TOKEN
-
 docker build -f Dockerfile --tag ${COMPONENT}:${IMAGE_VERSION} .
 docker tag ${COMPONENT}:${IMAGE_VERSION} ${DOCKER_IMAGE_URL}:${IMAGE_VERSION}
 docker push ${DOCKER_IMAGE_URL=}:${IMAGE_VERSION}
